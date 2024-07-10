c19d3dd9420e81ef442bada03ad9b4e427803dea
Alex test <gmodkru@gmail.com>
2024-07-10 15:36:27 +0300

    Task done

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,9 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        int ticketPrice = 53290;
+        int bonusScale = 20;
+        int bonusMile = ticketPrice / bonusScale;
 
-        }
+        System.out.println("При покупке билета стоимостью " + ticketPrice + " руб., Ваш бонусный счет составит " + bonusMile + " бонус миль.");
     }
 }
